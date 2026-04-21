533f392918b45343195220e8ffa13915d4082215
晴书烫月 <3364448953@qq.com>
2026-04-21 10:42:15 +0800

    更新

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        PlayerManager.Instance.RegisterPlayer(this);
+    }
+
+    private void OnDisable()
+    {
+        PlayerManager.Instance.UnregisterPlayer(this);
+    }
+
+}
