fef9249284d4bcbd55edf42b979e22adcefc31cb
晴书烫月 <3364448953@qq.com>
2026-04-25 00:11:07 +0800

    背包UI更新

--- Assets/Scripts/Data/BackpackItemData/BackpackItemDataSO.cs
+++ Assets/Scripts/Data/BackpackItemData/BackpackItemDataSO.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Setup/BackpackItem Data", fileName = "BackpackItemData - ")]
 public class BackpackItemDataSO : ScriptableObject
 {
-    public Sprite itemSprite;
-    public float pixelAmount = 100; // RectTranform的每格尺寸
+    public float pixelAmount = 80; // RectTranform的每格尺寸
     public Vector2Int imageSize = new Vector2Int(0, 0); // 道具图像应该长几格，宽几格
     public Vector2Int[] occupationArea ; // 道具实际所占格子
 }
---
+++ Assets/Scripts/Enum/SlotState.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SlotState 
+{
+    None,
+    EnablePlace,
+    DisablePlace,
+    EnableReplace
+}
