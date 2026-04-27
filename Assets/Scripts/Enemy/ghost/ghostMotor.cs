3ade89ce0c0b9d16f56e1ffd8875eb15775b4bba
noviciatelsy <noviiciate@gmail.com>
2026-04-27 23:06:56 +0800

    完成幽灵逻辑

---
+++ Assets/Scripts/Enemy/ghost/ghostMotor.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ghostMotor : IMonsterMotor
+{
+    public void Execute(MonsterBase owner, IIntent intent)
+    {
+        if (intent is not ChaseIntent chase)
+            return;
+
+        Ghost enemy = owner as Ghost;
+        if (enemy == null || chase.target == null)
+            return;
+
+        Vector2 dir = ((Vector2)chase.target.position - enemy.Position).normalized;
+        Debug.Log("onchase" + enemy.transform.position);
+        enemy.transform.position += (Vector3)(dir * enemy.moveSpeed * Time.fixedDeltaTime);
+    }
+}
