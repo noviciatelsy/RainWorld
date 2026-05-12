24bb1ee7e07f61a1f2ee04662afdeaa1cca3085b
晴书烫月 <3364448953@qq.com>
2026-05-12 09:15:37 +0800

    背包优化

--- Assets/Scripts/Data/BackpackItemData/BackpackItemDataSO.cs
+++ Assets/Scripts/Data/BackpackItemData/BackpackItemDataSO.cs
@@ -3,17 +3,12 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Setup/BackpackItem Data", fileName = "BackpackItemData - ")]
 public class BackpackItemDataSO : ScriptableObject
 {
-    public float pixelAmount = 80; // RectTranform的每格尺寸
     public Vector2Int imageSize = new Vector2Int(1, 1); // 道具图像应该长几格，宽几格
     public Vector2Int[] occupationArea; // 道具实际所占格子
 
 #if UNITY_EDITOR
     private void OnValidate()
     {
-        if (pixelAmount <= 0)
-        {
-            pixelAmount = 80;
-        }
 
         if (imageSize.x < 1)
         {
