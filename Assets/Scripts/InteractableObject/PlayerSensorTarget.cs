32f4829356e39435dad0335d999900a5ee1d3657
晴书烫月 <3364448953@qq.com>
2026-05-26 10:55:52 +0800

    更新上传

--- Assets/Scripts/InteractableObject/PlayerSensorTarget.cs
+++ Assets/Scripts/InteractableObject/PlayerSensorTarget.cs
@@ -1,11 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public abstract class PlayerSensorTarget : MonoBehaviour
 {
+
+    [SerializeField] protected TextMeshPro displayText;
+
+    private void Awake()
+    {
+        if(displayText!=null)
+        {
+            displayText.gameObject.SetActive(false);
+        }
+    }
     public virtual void Interact()
     {
 
+    }
+
+    protected virtual void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (displayText != null)
+            displayText.gameObject.SetActive(true);
+    }
+
+    protected virtual void OnTriggerExit2D(Collider2D collision)
+    {
+        if (displayText != null)
+            displayText.gameObject.SetActive(false);
     }
 }
